6c0586461dbfa202f715cd8b360075c709fe7e4f
Liszt21 <1832666492@qq.com>
2020-11-09 15:18:35 +0800

    sync modified in ps1

--- bin/lemacs.ps1
+++ bin/lemacs.ps1
@@ -1,25 +1,27 @@
-if ($ENV:USERNAME -eq 'liszt'){
+if ($ENV:USERNAME -eq 'liszt') {
     $ISME = $true
-}else{
+}
+else {
     $ISME = $false
 }
 
 if ($ENV:LEMACS) {
     $LEMACS = $ENV:LEMACS
-}else{
+}
+else {
     $LEMACS = "."
 }
 
 $Origin_Location = Get-Location
 
 function Debug-Lemacs($message) {
-    if ($DEBUG){
+    if ($DEBUG) {
         Write-Output "$message"
     }
 }
 
 function Update-Git {
-    if( !(Test-Path bin/lemacs.ps1)){
+    if ( !(Test-Path bin/lemacs.ps1)) {
         Write-Output "Not in Lemacs"
         return 
     }
@@ -30,19 +32,19 @@ function Update-Git {
     }
     Set-Location ..
     Remove-Item -r $folder
-    git clone https://github.com/Liszt21/Lemacs $folder
+    git clone --recursive https://github.com/Liszt21/Lemacs $folder
     Set-Location $folder
 }
 
 function Check {
-    if (!$ENV:LEMACS){
+    if (!$ENV:LEMACS) {
         if (!$(Get-Command lemacs)) {
             Write-Output "Lemacs isn't installed!"
 
-            if(!$(Get-Command emacs)){
+            if (!$(Get-Command emacs)) {
                 Write-Output "Emacs isn't installed!"
 
-                if(!$(Get-Command scoop)){
+                if (!$(Get-Command scoop)) {
                     Write-Output "Scoop isn't installed! Installing..."
                     Invoke-WebRequest -useb https://gitee.com/liszt21/AuTools/raw/master/script/system/init/windows.ps1 | Invoke-Expression
                     scoop install emacs
@@ -58,17 +60,18 @@ function Check {
         
         if ($ISME) {
             Set-Location ..
-            if(Test-Path C:/Liszt/Projects/Lemacs){
+            if (Test-Path C:/Liszt/Projects/Lemacs) {
                 Remove-Item -r master
-            }else{
+            }
+            else {
                 Move-Item master C:/Liszt/Projects/Lemacs
             }
             sudo ln -s C:/Liszt/Projects/Lemacs ./master
         }
-        [environment]::setEnvironmentVariable('LEMACS',$ENV:LEMACS,'User')
+        [environment]::setEnvironmentVariable('LEMACS', $ENV:LEMACS, 'User')
     }
     Set-Location $LEMACS
-    if (!(Test-Path .git)){
+    if (!(Test-Path .git)) {
         Write-Debug "lemacs isn't a git folder"
         Update-Git
     }
@@ -80,9 +83,10 @@ function Help {
 
 function Update {
     Write-Output "Updating"
-    git pull origin master
-    git submodule init
-    git submodule update
+    # git pull origin master
+    # git submodule init
+    # git submodule update
+    git submodule foreach git pull
 }
 
 function Install {
@@ -94,7 +98,7 @@ function Install {
     sudo ln -s ($LEMACS + "/config/.spacemacs.d") ("C:/Users/$ENV:USERNAME/AppData/Roaming/.spacemacs.d")
 }
 
-function Clean{
+function Clean {
     Write-Output "Clean"
     Remove-Item ("C:/Users/$ENV:USERNAME/AppData/Roaming/.emacs")
     Remove-Item ("C:/Users/$ENV:USERNAME/AppData/Roaming/.doom.d")
@@ -104,32 +108,33 @@ function Clean{
 }
 
 function Run($command) {
-    if(!$command){
+    if (!$command) {
         $command = "Default"
     }
-    switch -regex ($command){
+    switch -regex ($command) {
         "Default" {
             Write-Output "Default"
-            Start-Job {emacs}
+            Start-Job { emacs }
             return
         }
         "^d" {
             Write-Output "Doomemacs"
-            Start-Job {emacs --with-profile doomemacs}
+            Start-Job { emacs --with-profile doomemacs }
         }
         "^s" {
             Write-Output "Spacemacs"
-            Start-Job {emacs --with-profile spacemacs}
+            Start-Job { emacs --with-profile spacemacs }
         }
     }
 }
 
 Check
 
-if (!$args -or !$args[0] -eq "update"){
+if (!$args -or !$args[0] -eq "update") {
     Update
-} else {
-    switch ($args[0]){
+}
+else {
+    switch ($args[0]) {
         'doom' {
             Write-Output "Doom"
             .\config\doomemacs\bin\doom.cmd $args[1..$args.Count]
