7f36da1ef8e16d8f066695591d88d78ea7b0f9ce
liszt21 <1832666492@qq.com>
2021-04-20 11:06:41 +0800

    fix profile name error

--- src/entry.sh
+++ src/entry.sh
@@ -1,4 +1,4 @@
-LEMACS_HOME=${LEMACS_HOME:-~/Apps/lemacs}
+LEMACS_HOME=${LEMACS_HOME:-~/Lemacs}
 export LEMACS_HOME
 
 export PATH=$LEMACS_HOME/bin:$LEMACS_HOME/config/doomemacs/bin:$PATH
--- src/profile.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/doomemacs")))
- ("doomemacs" . ((user-emacs-directory . "~/doomemas")))
- ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
- )
