9850c2c95735200a69391640fe5d9e9a81181de1
liszt21 <liszt21@qq.com>
2022-10-20 16:10:44 +0800

    feat: add profiles

---
+++ configs/doomemacs/profiles.el
@@ -0,0 +1,10 @@
+((doomemacs
+   (user-emacs-directory . "~/Emacy/library/doomemacs")
+   ("DOOMDIR" . "~/Emacy/configs/doomemacs"))
+ (spacemacs
+  (user-emacs-directory . "~/Emacy/library/spacemacs")
+  ("SPACEMACSDIR" . "~/Emacy/configs/spacemacs"))
+ (doomemacs-origin
+   (user-emacs-directory . "~/Emacy/library/doomemacs"))
+ (spacemacs-origin
+  (user-emacs-directory . "~/Emacy/library/spacemacs")))
