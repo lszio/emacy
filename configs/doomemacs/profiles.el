4315dd4774b7f4365aa3aa1f1fe49870e4d1ca1a
liszt21 <liszt21@qq.com>
2022-11-01 16:08:29 +0800

    feat(profile): add centaur & vanilla profile

--- configs/doomemacs/profiles.el
+++ configs/doomemacs/profiles.el
@@ -4,6 +4,10 @@
  (spacemacs
   (user-emacs-directory . "~/Emacy/library/spacemacs")
   ("SPACEMACSDIR" . "~/Emacy/configs/spacemacs"))
+ (centaur
+  (user-emacs-directory . "~/Emacy/library/centaur"))
+ (vanilla
+  (user-emacs-directory . "~/Emacy/library/vanilla"))
  (doomemacs-origin
    (user-emacs-directory . "~/Emacy/library/doomemacs"))
  (spacemacs-origin
