db46b1d08fdf52c4e56f185f6fe6c94585ad815d
liszt21 <liszt21@qq.com>
2022-11-15 14:16:26 +0800

    feat(doom): add hammy

--- configs/doomemacs/modules/private/misc/config.el
+++ configs/doomemacs/modules/private/misc/config.el
@@ -1,1 +1,12 @@
-;; (use-package! pinentry :config (pinentry-start))
+(use-package! hammy
+  :config
+  (hammy-define (propertize "12" 'face '(:foreground))
+    :documentation "Docs"
+    :intervals
+    (list
+     (interval :name "Work"
+               :duration "25 minutes"
+               :before (do (announce "start work")
+                           (notify "start work"))
+               :advance (do (announce "break work")
+                            (notify "break work"))))))
