6bc61e398bbdd89cb6da1ab30d41eef4dfd0053e
liszt21 <liszt21@qq.com>
2022-12-12 00:30:33 +0800

    feat(doom): add eaf-interleave

--- configs/doomemacs/modules/private/eaf/config.el
+++ configs/doomemacs/modules/private/eaf/config.el
@@ -1,18 +1,5 @@
 (use-package! eaf)
-;; (use-package! eaf-evil
-;;   :config
-;;   (define-key key-translation-map (kbd "SPC")
-;;       (lambda (prompt)
-;;         (if (derived-mode-p 'eaf-mode)
-;;             (pcase eaf--buffer-app-name
-;;               ("browser" (if  eaf-buffer-input-focus
-;;                              (kbd "SPC")
-;;                            (kbd eaf-evil-leader-key)))
-;;               ("pdf-viewer" (kbd eaf-evil-leader-key))
-;;               ("image-viewer" (kbd eaf-evil-leader-key))
-;;               (_  (kbd "SPC")))
-;;           (kbd "SPC")))))
-(use-package! eaf-all-the-icons)
+
 ;; apps
 (use-package! eaf-browser)
 (use-package! eaf-jupyter)
@@ -35,5 +22,26 @@
 ;; (use-package! eaf-demo)
 ;; (use-package! eaf-vue-demo)
 
+(use-package! eaf-all-the-icons)
 (use-package! eaf-evil)
+(use-package! eaf-interleave
+  :custom
+  (eaf-find-alternate-file-in-dired t)
+  :diminish eaf-mode
+  :bind (:map eaf-interleave-mode-map
+         ("M-." . 'eaf-interleave-sync-current-note)
+         ("M-p" . 'eaf-interleave-sync-previous-note)
+         ("M-n" . 'eaf-interleave-sync-next-note)
+         :map eaf-interleave-app-mode-map
+         ("C-c M-i" . 'eaf-interleave-add-note)
+         ("C-c M-o" . 'eaf-interleave-open-notes-file)
+         ("C-c M-q" . 'eaf-interleave-quit))
+  :config
+  (add-hook 'eaf-pdf-viewer-hook 'eaf-interleave-app-mode)
+  (add-hook 'eaf-browser-hook 'eaf-interleave-app-mode)
+  (add-hook 'org-mode-hook 'eaf-interleave-mode)
+  (setq eaf-interleave-org-notes-dir-list '("~/Notes/content"))
+  (setq eaf-interleave-split-direction 'vertical)
+  (setq eaf-interleave-disable-narrowing t)
+  (setq eaf-interleave-split-lines 20))
 (setq eaf-evil-leader-key "SPC")
