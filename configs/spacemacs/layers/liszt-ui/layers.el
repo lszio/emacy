15c17ed112e5ed6ae6d07b5e3511bc83bd0dd26e
liszt21 <liszt21@qq.com>
2023-12-03 21:36:53 +0800

    refactor: clean all

--- configs/spacemacs/layers/liszt-ui/layers.el
+++
@@ -1,9 +0,0 @@
-(configuration-layer/declare-layers
- '(
-   unicode-fonts
-   ;; (unicode-fonts :variables unicode-fonts-enable-ligatures t)
-   (tabs :variables
-         tabs-auto-hide t
-        ;; tabs-auto-hide-delay 3
-	       )
-   ))
