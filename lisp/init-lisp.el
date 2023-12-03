1407925fb033a1b356d33c38e2a8b94af3406d99
liszt21 <liszt21@qq.com>
2023-12-03 14:21:32 +0800

    feat: add slime & parinfer-rust-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -12,6 +12,18 @@
 
 (use-package cider)
 
+(use-package slime
+  :general
+  (:keymaps 'slime-mode-map
+            "C-<return>" 'slime-eval-last-expression)
+  :config
+  (setq inferior-lisp-program "ros run"))
+
+(use-package parinfer-rust-mode
+  :hook '(emacs-lisp-mode lisp-mode)
+  :config
+  (electric-pair-mode -1))
+
 (provide 'init-lisp)
 
 ;;; init-lisp.el ends here
